5145692f5c5365ba98698ba82888b0561e2e4ac5
Vetle Engesbak <vetleengesbak@gmail.com>
2013-03-23 10:17:45 +0100

    adding all urls caused a NameError because there were no corresponding views, tried to add those now..

--- TODOs/views.py
+++ TODOs/views.py
@@ -13,3 +13,22 @@ from django.shortcuts import render_to_response, redirect
 def view_todos(request):
     template = "base.html"
     return render_to_response(template)
+
+def tickedTodosView(request, page_number=1):
+    return HttpResponse("tickedTodosView")
+def addTodoView(request):
+    return HttpResponse("tickedTodosView")
+def tickTodoView(request, todoId):
+    return HttpResponse("tickedTodosView")
+def unTickTodoView(request, todoId):
+    return HttpResponse("tickedTodosView")
+def deleteTodoView(request, todoId):
+    return HttpResponse("tickedTodosView")
+def loginView(request):
+    return HttpResponse("tickedTodosView")
+def logoutView(request):
+    return HttpResponse("tickedTodosView")
+def newUserView(request):
+    return HttpResponse("tickedTodosView")
+def createUserView(request):
+    return HttpResponse("tickedTodosView")
