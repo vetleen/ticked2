582151b26f08e48726f80b1b2a3c289885372d4f
Vetle Engesbak <vetleengesbak@gmail.com>
2013-03-23 10:00:12 +0100

    added simple view and url to point to that view

--- TODOs/views.py
+++ TODOs/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+##from django.template import Context, RequestContext
+##from django.template.loader import get_template
+##from django.contrib.auth.models import User
+##from django.contrib.auth import authenticate, login, logout
+##from django.core.context_processors import csrf
+##from django.shortcuts import render_to_response, redirect
+
+
+def view_todos (request):
+	return HttpResponse("hello ticked2")
+	
--- ticked2/urls.py
+++ ticked2/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from TODOs.views import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', view_todos),
     # Examples:
     # url(r'^$', 'ticked2.views.home', name='home'),
     # url(r'^ticked2/', include('ticked2.foo.urls')),
