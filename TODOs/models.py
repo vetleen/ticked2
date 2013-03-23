292b0af06d3f3d8c0a160705969ea0b968a0e7cf
Vetle Engesbak <vetleengesbak@gmail.com>
2013-03-23 10:26:16 +0100

    added models.py

--- TODOs/models.py
+++ TODOs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Todo(models.Model):
+	headLine = models.CharField(max_length=50, blank=True)
+	bodyText = models.CharField(max_length=300, blank=True)
+	owner = models.ForeignKey(User)
+	todoIsTicked = models.BooleanField(default=False)
