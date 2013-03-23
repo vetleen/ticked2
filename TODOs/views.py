b033cb5e6784bd9f05a06bd56fcf6c059f2be9c5
Vetle Engesbak <vetleengesbak@gmail.com>
2013-03-23 10:21:43 +0100

    added all imports to views.py

--- TODOs/views.py
+++ TODOs/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-##from django.template import Context, RequestContext
 from django.template.loader import get_template
-##from django.contrib.auth.models import User
-##from django.contrib.auth import authenticate, login, logout
-##from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, redirect
+
+from django.template import Context, RequestContext
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.core.context_processors import csrf
 
 
 ##def view_todos (request):
