01c2410cab75b0e782f71bb5c234811330180609
Vetle Engesbak <vetleengesbak@gmail.com>
2013-03-23 10:06:15 +0100

    fix

--- TODOs/views.py
+++ TODOs/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 ##from django.template import Context, RequestContext
-##from django.template.loader import get_template
+from django.template.loader import get_template
 ##from django.contrib.auth.models import User
 ##from django.contrib.auth import authenticate, login, logout
 ##from django.core.context_processors import csrf
-##from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect
 
 
-def view_todos (request):
-    return HttpResponse("hello ticked2")
-    
+##def view_todos (request):
+##    return HttpResponse("hello ticked2")
+##    
+def view:rodos(request):
+    template = "base.html"
+    return render_to_response(template)
